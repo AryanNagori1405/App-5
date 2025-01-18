ce58a12edbb97e25d3aa45ab840af74c740de280
ARYAN <aryannikill123@gmail.com>
2025-01-19 00:34:29 +0530

    Add header, total price, company and logo.

--- main.py
+++ main.py
@@ -44,4 +44,23 @@ for filepath in filepaths:
         pdf.cell(w=30, h=10, txt=str(row["price_per_unit"]), border=1)
         pdf.cell(w=30, h=10, txt=str(row["total_price"]), border=1, ln=1)
 
+    total_sum = df["total_price"].sum()
+
+    pdf.set_font(family="Times", size=12)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=10, txt="", border=1)
+    pdf.cell(w=60, h=10, txt="", border=1)
+    pdf.cell(w=40, h=10, txt="", border=1)
+    pdf.cell(w=30, h=10, txt="", border=1)
+    pdf.cell(w=30, h=10, txt=str(total_sum), border=1, ln=1)
+
+    # Add total sum sentence
+    pdf.set_font(family="Times", size=18, style="B")
+    pdf.cell(w=30, h=25, txt=f"The total price is {total_sum}", ln=1)
+
+    # Add company name and logo
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=30, h=10, txt="PythonHow")
+    pdf.image("pythonhow.png", w=10)
+
     pdf.output(f"PDFs/{filename}.pdf")
